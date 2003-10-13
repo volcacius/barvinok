f773164110ba0c1718ffd7876455ca70805441d4
skimo <skimo>
2003-10-13 12:38:37 +0000

    allow include in C++ code

--- util.h
+++ util.h
@@ -1,9 +1,17 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 #include <polylib/polylibgmp.h>
 
 Polyhedron* Polyhedron_Polar(Polyhedron *P, unsigned NbMaxRays);
 Polyhedron* supporting_cone(Polyhedron *P, int v, unsigned NbMaxRays);
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
