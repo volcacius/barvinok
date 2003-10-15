27a6865c4cfced646f2e0776015330c961af572a
skimo <skimo>
2003-10-15 15:35:02 +0000

    The new ray should be multiplied with the constraints rather than the rays to check whether it is inside the polyhedron !

--- barvinok.cc
+++ barvinok.cc
@@ -189,8 +189,9 @@ public:
 	Vector *z = zz2vector(U[index]);
 	Value tmp;
 	value_init(tmp);
-	for (int i = 0; i < Rays->NbRows; ++i) {
-	    Inner_Product(z->p, Rays->p[i]+1, z->Size-1, &tmp);
+	Polyhedron *C = poly();
+	for (int i = 0; i < C->NbConstraints; ++i) {
+	    Inner_Product(z->p, C->Constraint[i]+1, z->Size-1, &tmp);
 	    if (value_neg_p(tmp)) {
 		value_set_si(tmp, -1);
 		Vector_Scale(z->p, z->p, tmp, z->Size-1);
