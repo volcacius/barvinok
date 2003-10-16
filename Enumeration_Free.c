21af338723f44ce360b7222f068b716a003a9cc8
skimo <skimo>
2003-10-16 09:20:31 +0000

    provide our own Enumeration_Free

---
+++ Enumeration_Free.c
@@ -0,0 +1,13 @@
+void Enumeration_Free(Enumeration *en)
+{
+  Enumeration *ee;
+
+  while( en )
+  {
+	  free_evalue_refs( &(en->EP) );
+	  Polyhedron_Free( en->ValidityDomain );
+	  ee = en ->next;
+	  free( en );
+	  en = ee;
+  }
+}
