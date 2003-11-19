95fa0b382fd428034c2e003fd1a5d9b33d0978b0
skimo <skimo>
2003-11-19 17:42:07 +0000

    add main program for enumerate

---
+++ barvinok_enumerate.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <sys/times.h>
+#include <polylib/polylibgmp.h>
+#include <util.h>
+#include <barvinok.h>
+
+int main()
+{
+    Polyhedron *A, *C;
+    Matrix *M;
+    Enumeration *en;
+    char **param_name;
+
+    M = Matrix_Read();
+    A = Constraints2Polyhedron(M, 600);
+    Matrix_Free(M);
+    M = Matrix_Read();
+    C = Constraints2Polyhedron(M, 600);
+    Polyhedron_Print(stdout, P_VALUE_FMT, A);
+    Polyhedron_Print(stdout, P_VALUE_FMT, C);
+    param_name = Read_ParamNames(stdin, C->Dimension);
+    en = barvinok_enumerate(A, C, 600);
+    print_evalue(stdout, &en->EP, param_name);
+    Enumeration_Free(en);
+    Polyhedron_Free(A);
+    Polyhedron_Free(C);
+    return 0;
+}
