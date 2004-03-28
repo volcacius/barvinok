25c3d29ce2624a95e94ec33830e6bd5b378c34da
skimo <skimo>
2004-03-28 15:42:27 +0000

    export _ev version

--- barvinok.h
+++ barvinok.h
@@ -12,6 +12,7 @@ extern "C" {
 void barvinok_decompose(Polyhedron *C, Polyhedron **ppos, Polyhedron **pneg);
 void barvinok_count(Polyhedron *P, Value* result, unsigned NbMaxCons);
 Enumeration* barvinok_enumerate(Polyhedron *P, Polyhedron* C, unsigned MaxRays);
+evalue* barvinok_enumerate_ev(Polyhedron *P, Polyhedron* C, unsigned MaxRays);
 evalue* barvinok_enumerate_e(Polyhedron *P, 
 			  unsigned exist, unsigned nparam, unsigned MaxRays);
 
