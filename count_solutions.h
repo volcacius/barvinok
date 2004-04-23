f9df8bc565bb833992adb9fa21b20a5da9cbd4cc
skimo <skimo>
2004-04-23 14:02:47 +0000

    allow inclusion from C++

--- count_solutions.h
+++ count_solutions.h
@@ -1,8 +1,12 @@
 /* Erin Parker (parker@cs.unc.edu), March 2004 */
 
 #include <stdlib.h>
-#include "dfa.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "dfa.h"
 
 /* Functions defined in construction.c */
 DFA* build_DFA_eq(int, int*, int, int*);
@@ -10,3 +14,7 @@ DFA* build_DFA_ineq(int, int*, int, int*);
 
 /* Function defined in count.c */
 void count_accepting_paths(DFA*, int, int);
+
+#ifdef __cplusplus
+}
+#endif
