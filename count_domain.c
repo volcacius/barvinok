a2a3cd25ef91a8f06b0c0386cf322af5d0d35d4c
skimo <skimo>
2004-07-05 12:09:05 +0000

    preload library to count the number of validity domains

---
+++ count_domain.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <dlfcn.h>
+#include "ev_operations.h"
+#include "barvinok.h"
+
+/* gcc -shared  count_domain.c -rdynamic -o libcount.so -ldl -lc */
+
+evalue* barvinok_enumerate_ev(Polyhedron *P, Polyhedron* C, unsigned MaxRays)
+{
+    static evalue *(*orig)(Polyhedron *, Polyhedron *c, unsigned) = NULL;
+    evalue *res;
+    int c;
+
+    if (!orig) {
+	void *handle = dlopen("libbarvinok.so", RTLD_LAZY);
+	assert(handle);
+	orig = dlsym(handle, "barvinok_enumerate_ev");
+	assert(orig);
+	dlclose(handle);
+    }
+
+    res = orig(P, C, MaxRays);
+
+    c = value_notzero_p(res->d) ? 0
+	: res->x.p->type == partition ? res->x.p->size/2 : 1;
+    fprintf(stderr, "COUNT: %d %d\n", P->Dimension, c);
+
+    return res;
+}
