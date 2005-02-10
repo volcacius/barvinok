a0539b52cf2d3b7dffdbbbe3894be836cbe6bb29
skimo <skimo>
2005-02-10 11:39:25 +0000

    directly check series

--- verify_main.cc
+++ verify_main.cc
@@ -43,6 +43,90 @@ struct option options[] = {
 /* if dimension >= VBIDDIM, use VSRANGE */
 #define VBIGDIM 8
 
+int check_series(Polyhedron *S,Polyhedron *C, gen_fun *gf,
+	         int nparam,int pos,Value *z)
+{
+    int k;
+    Value c, tmp;
+    value_init(c);
+    value_init(tmp);
+
+    if(pos == nparam) {
+	/* Computes the coefficient */
+	gf->coefficient(&z[S->Dimension-nparam+1], &c);
+
+	/* if c=0 we may be out of context. */
+	/* scanning is useless in this case*/
+	if(!in_domain(C,&z[S->Dimension-nparam+1])) {
+	    /* ok */ ;
+	} else {
+
+#ifdef PRINT_ALL_RESULTS
+	    printf("EP( ");
+	    value_print(stdout,VALUE_FMT,z[S->Dimension-nparam+1]);
+	    for(k=S->Dimension-nparam+2;k<=S->Dimension;++k) {
+		printf(", ");
+		value_print(stdout,VALUE_FMT,z[k]);
+	    }
+	    printf(" ) = ");
+	    value_print(stdout,VALUE_FMT,c);
+	    printf(" ");
+#endif
+	    /* Manually count the number of points */
+	    count_points(1,S,z,&tmp);
+#ifdef PRINT_ALL_RESULTS
+	    printf(", count = ");
+	    value_print(stdout, P_VALUE_FMT, tmp);
+	    printf(". ");
+#endif
+
+	    if(value_ne(tmp,c)) {
+		printf("\n"); 
+		fflush(stdout);
+		fprintf(stderr,"Error !\n");
+		fprintf(stderr,"EP( ");
+		value_print(stderr,VALUE_FMT,z[S->Dimension-nparam+1]);
+		for(k=S->Dimension-nparam+2;k<=S->Dimension;++k) {
+		  fprintf(stderr,", ");
+		  value_print(stderr,VALUE_FMT,z[k]);
+		}
+		fprintf(stderr," ) should be ");
+		value_print(stderr,VALUE_FMT,tmp);
+		fprintf(stderr,", while EP eval gives ");
+		value_print(stderr,VALUE_FMT,c);
+		fprintf(stderr,".\n");
+#ifndef DONT_BREAK_ON_ERROR
+		value_clear(c); value_clear(tmp);
+		return 0;
+#endif
+	    }
+#ifdef PRINT_ALL_RESULTS
+	    else
+		printf("OK.\n");
+#endif
+	}
+    } else
+	for(value_assign(tmp,Min); value_le(tmp,Max); 
+				   value_increment(tmp,tmp)) {
+#ifndef PRINT_ALL_RESULTS
+	  k = VALUE_TO_INT(tmp);
+	  if(!pos && !(k%st)) {
+	    printf("o");
+	    fflush(stdout);
+	  }
+#endif
+	  value_assign(z[pos+S->Dimension-nparam+1],tmp);
+	  if(!check_series(S,C,gf,nparam,pos+1,z)) {
+	    value_clear(c); value_clear(tmp);
+	    return(0);
+	  }
+	}
+
+    value_clear(c);
+    value_clear(tmp);
+    return 1;
+}
+
 int main(int argc,char *argv[]) {
 	
   Matrix *C1, *P1;
@@ -55,6 +139,7 @@ int main(int argc,char *argv[]) {
     int c, ind = 0;
     int series = 0;
     int verbose = 0;
+    int result = 0;
 
 /******* Read the input *********/
   P1 = Matrix_Read();
@@ -127,12 +212,13 @@ int main(int argc,char *argv[]) {
   Domain_Free(C);
   C = CC;
 
+    gen_fun *gf = 0;
+
     /******* Compute EP *********/
     if (!series)
         en = barvinok_enumerate(P,C,MAXRAYS);
     else {
 	evalue *EP;
-	gen_fun *gf;
 	gf = barvinok_series(P, C, MAXRAYS);
 	if (verbose) {
 	    gf->print(C->Dimension, params);
@@ -168,14 +254,19 @@ int main(int argc,char *argv[]) {
   }
 #endif
 
-  /******* CHECK NOW *********/
-  if(S && !check_poly(S,C,en,C->Dimension,0,p)) {
-    fprintf(stderr,"Check failed !\n");
-    for(i=0;i<=(P->Dimension+1);i++) 
-      value_clear(p[i]);
-    value_clear(tmp);  
-    return(-1);
-  }
+    /******* CHECK NOW *********/
+    if(S) {
+	if (!series) {
+	    if (!check_poly(S,C,en,C->Dimension,0,p))
+		result = -1;
+	} else {
+	    if (!check_series(S,C,gf,C->Dimension,0,p))
+		result = -1;
+	}
+    }
+
+    if (result == -1)
+	fprintf(stderr,"Check failed !\n");
     
 #ifndef PRINT_ALL_RESULTS
   printf( "\n" );
@@ -185,7 +276,7 @@ int main(int argc,char *argv[]) {
     value_clear(p[i]);
   value_clear(tmp);
   Free_ParamNames(params, C->Dimension);
-  return(0);
+  return result;
 } /* main */
 
 
