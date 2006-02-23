d9cf25f36a8580f8629c01674d292cf87f9705e1
Sven Verdoolaege <skimo@kotnet.org>
2006-02-23 19:55:37 +0100

    clean up header files

--- src/bernstein++.h
+++ src/bernstein++.h
@@ -4,6 +4,8 @@
  *	- c to c++ functions
  */
 
+#include <string.h>
+#include <ginac/ginac.h>
 #include <gmp.h>
 extern "C" {
 #define matrix polylib_matrix
@@ -13,23 +15,26 @@ extern "C" {
 #undef polynomial
 }
 
-unsigned int findMaxDegree(ex &polynomial, matrix &Vars, unsigned int nbVar);
+unsigned int findMaxDegree(GiNaC::ex &polynomial, GiNaC::matrix &Vars, 
+			   unsigned int nbVar);
 
-int bernsteinExpansion(matrix &P, ex &poly, matrix &V, unsigned int nbVar
-		       , unsigned int nbVert, unsigned int maxDegree
-		       , matrix &Params, unsigned int nbParams);
+int bernsteinExpansion(GiNaC::matrix &P, GiNaC::ex &poly, GiNaC::matrix &V, 
+		       unsigned int nbVar, unsigned int nbVert, 
+		       unsigned int maxDegree, GiNaC::matrix &Params, 
+		       unsigned int nbParams);
 matrix getParameterMatrix(unsigned int nbParams, char **param_values);
 void getVariablesMatrix(unsigned int nbVariables);
-string int2String(int n);
-ex convertPolynomial(long long *m, unsigned int nbRows, unsigned int nbColumns, matrix &Vars, unsigned int nbVariables);
-
-extern "C" int polyConvertParameters(long long *m, unsigned int nbRows, unsigned int nbColumns
-				     , long long **llPolynomialCoefficients, unsigned int *llRows, unsigned int *llColumns
-				     , unsigned int nbParams, char **param_values);
-extern "C" int polyConvert(long long *m, unsigned int nbRows, unsigned int nbColumns, unsigned int nbParams, char **param_values);
+std::string int2String(int n);
+GiNaC::ex convertPolynomial(long long *m, unsigned int nbRows, unsigned int nbColumns, matrix &Vars, unsigned int nbVariables);
 
 extern "C" {
 
+int polyConvertParameters(long long *m, unsigned int nbRows, unsigned int nbColumns, 
+			  long long **llPolynomialCoefficients, unsigned int *llRows, 
+			  unsigned int *llColumns, unsigned int nbParams, 
+			  char **param_values);
+int polyConvert(long long *m, unsigned int nbRows, unsigned int nbColumns, 
+	        unsigned int nbParams, char **param_values);
 void doExpansion(Param_Polyhedron *PP, Param_Domain *Q, unsigned int nb_param, 
 		 char **param_name);
 
--- src/bernstein.h
+++ src/bernstein.h
@@ -7,6 +7,10 @@
 
 #define MAXRAYS 1000
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 extern int polyConvertParameters(long long *m, unsigned int nbRows, unsigned int nbColumns
 				     , long long **llPolynomialCoefficients, unsigned int *llRows, unsigned int *llColumns
 				     , unsigned int nbParams, char **param_values);
@@ -19,3 +23,8 @@ unsigned  checkConstraint(long long *M, unsigned int rows, unsigned int columns)
 Matrix *longlong2polylib(long long *M, unsigned int rows, unsigned int columns);
 void Matrix_Set(int r, int c, int v, Matrix * A);
 
+void printDomain(Param_Domain *Q, Polyhedron *B, char **param_name);
+
+#if defined(__cplusplus)
+}
+#endif
