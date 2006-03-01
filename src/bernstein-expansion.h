f40ae0f293e4fa9b8ba7f9a3c145c41cc7a9a82f
Sven Verdoolaege <skimo@kotnet.org>
2006-03-01 11:43:34 +0100

    Move max/min coefficient computation to main.cpp

--- src/bernstein-expansion.h
+++ src/bernstein-expansion.h
@@ -19,19 +19,3 @@ GiNaC::matrix domainVertices(Param_Polyhedron *PP, Param_Domain *Q,
 GiNaC::lst bernsteinExpansion(const GiNaC::matrix &vert, GiNaC::ex poly, 
 			      const GiNaC::exvector& vars,
 			      const GiNaC::exvector& params);
-int getMaxMinCoefficient(Polyhedron *VD, GiNaC::lst coeffs, 
-			const GiNaC::exvector& Params);
-void generateMaxConstraints(GiNaC::lst coeffs, const GiNaC::exvector& Params);
-GiNaC::ex constantTerm(GiNaC::ex poly, const GiNaC::exvector &Params);
-GiNaC::ex calculateLCM(GiNaC::ex poly, const GiNaC::exvector &Params, GiNaC::ex &cte);
-bool generateMaxConstraints(Polyhedron *VD, GiNaC::lst coeffs, 
-			    const GiNaC::exvector &Params, unsigned int max);
-bool generateMinConstraints(Polyhedron *VD, GiNaC::lst coeffs, 
-			    const GiNaC::exvector &Params, unsigned int min);
-bool generatePositiveNegativeConstraints(Polyhedron *VD, bool positive);
-GiNaC::ex getMaxMinCoefficient1Param(GiNaC::lst coeffs, unsigned int maxDegree, 
-				     GiNaC::ex Param, bool max, bool positive);
-bool calculateDirection(bool max, bool positive, bool even);
-void ex2longlongRow(long long *M, unsigned int row, polynomial &difference,
-		    const GiNaC::exvector& Params);
-
