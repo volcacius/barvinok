volcacius/barvinok

commit 2fa98813fdbcab1e55a9fb699343196f6c01ec56

barvinok_maximize: fix access to freed memory
This problem was introduced in 91195d52675fbcb4db1fd598747eaaa9b10a606d.

maximize.cc
@@ -686,6 +686,7 @@ static struct constraint *evalue_read_domain(struct stream *s, struct parameter 
     while (evalue_read_constraint(s, p, &constraints, union_next)) {
 	tok = stream_next_token(s);
 	if (tok) {
+	    line = tok->line;
 	    if (tok->type == TOKEN_UNION) {
 		token_free(tok);
 		union_next = constraints;
@@ -697,7 +698,6 @@ static struct constraint *evalue_read_domain(struct stream *s, struct parameter 
 		if (tok->line > line+1)
 		    break;
 	    }
-	    line = tok->line;
 	}
     }
     return constraints;

commit 3a51273a71975d9558f32755247a9b34ca552900

barvinok_maximize: fix detection of end of domain specification
The detection was broken in 2fa98813fdbcab1e55a9fb699343196f6c01ec56.

maximize.cc
@@ -689,8 +689,8 @@ static struct constraint *evalue_read_domain(struct stream *s, struct parameter 
     while (evalue_read_constraint(s, p, &constraints, union_next)) {
 	tok = stream_next_token(s);
 	if (tok) {
-	    line = tok->line;
 	    if (tok->type == TOKEN_UNION) {
+		line = tok->line;
 		token_free(tok);
 		union_next = constraints;
 		constraints = NULL;
@@ -700,6 +700,7 @@ static struct constraint *evalue_read_domain(struct stream *s, struct parameter 
 		/* empty line separates domain from evalue */
 		if (tok->line > line+1)
 		    break;
+		line = tok->line;
 	    }
 	}
     }