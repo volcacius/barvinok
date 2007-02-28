volcacius/barvinok

commit 3a51273a71975d9558f32755247a9b34ca552900

barvinok_maximize: fix detection of end of domain specification
The detection was broken in 2fa98813fdbcab1e55a9fb699343196f6c01ec56.

maximize.cc
@@ -689,8 +689,8 @@ static struct constraint *evalue_read_domain(struct stream *s, struct parameter 
     while (evalue_read_constraint(s, p, &constraints, union_next)) {
 	tok = stream_next_token(s);
 	if (tok) {
-	    line = tok->line;
 	    if (tok->type == TOKEN_UNION) {
+		line = tok->line;
 		token_free(tok);
 		union_next = constraints;
 		constraints = NULL;
@@ -700,6 +700,7 @@ static struct constraint *evalue_read_domain(struct stream *s, struct parameter 
 		/* empty line separates domain from evalue */
 		if (tok->line > line+1)
 		    break;
+		line = tok->line;
 	    }
 	}
     }

commit 07c533c5881219617fab75985f650919fd85a860

barvinok_maximize: fix handling of UNIONs in domains.
Broken in 3a51273a71975d9558f32755247a9b34ca552900.

maximize.cc
@@ -690,8 +690,13 @@ static struct constraint *evalue_read_domain(struct stream *s, struct parameter 
 	tok = stream_next_token(s);
 	if (tok) {
 	    if (tok->type == TOKEN_UNION) {
-		line = tok->line;
 		token_free(tok);
+		tok = stream_next_token(s);
+		if (!tok) {
+		    stream_error(s, NULL, "unexpected EOF");
+		    return constraints;
+		}
+		stream_push_token(s, tok);
 		union_next = constraints;
 		constraints = NULL;
 	    } else {
@@ -700,8 +705,8 @@ static struct constraint *evalue_read_domain(struct stream *s, struct parameter 
 		/* empty line separates domain from evalue */
 		if (tok->line > line+1)
 		    break;
-		line = tok->line;
 	    }
+	    line = tok->line;
 	}
     }
     return constraints;