volcacius/barvinok

commit 323ee0f57c8583146b0ecd737217671b98b4896d

barvinok_enumerate_union_series: call core of barvinok_series
We want to take advantage of the preprocessing performed by barvinok_series.

barvinok.cc
@@ -3512,16 +3512,11 @@ static Polyhedron *remove_more_equalities(Polyhedron *P, unsigned nparam,
 }
 #endif
 
-gen_fun * barvinok_series(Polyhedron *P, Polyhedron* C, unsigned MaxRays)
+/* frees P */
+static gen_fun *series(Polyhedron *P, unsigned nparam, unsigned MaxRays)
 {
     Matrix *CP = NULL;
-    Polyhedron *CA;
-    unsigned nparam = C->Dimension;
     gen_fun *gf;
-
-    CA = align_context(C, P->Dimension, MaxRays);
-    P = DomainIntersection(P, CA, MaxRays);
-    Polyhedron_Free(CA);
 
     if (emptyQ2(P)) {
 	Polyhedron_Free(P);
@@ -3550,14 +3545,30 @@ gen_fun * barvinok_series(Polyhedron *P, Polyhedron* C, unsigned MaxRays)
 
     gf_base *red;
     red = gf_base::create(Polyhedron_Project(P, nparam), P->Dimension, nparam);
+    POL_ENSURE_VERTICES(P);
     red->start_gf(P, MaxRays);
-    Polyhedron_Free(P);
     if (CP) {
 	red->gf->substitute(CP);
 	Matrix_Free(CP);
     }
     gf = red->gf;
     delete red;
+    Polyhedron_Free(P);
+    return gf;
+}
+
+gen_fun * barvinok_series(Polyhedron *P, Polyhedron* C, unsigned MaxRays)
+{
+    Polyhedron *CA;
+    unsigned nparam = C->Dimension;
+    gen_fun *gf;
+
+    CA = align_context(C, P->Dimension, MaxRays);
+    P = DomainIntersection(P, CA, MaxRays);
+    Polyhedron_Free(CA);
+
+    gf = series(P, nparam, MaxRays);
+
     return gf;
 }
 
@@ -3624,18 +3635,14 @@ gen_fun* barvinok_enumerate_union_series(Polyhedron *D, Polyhedron* C,
     D2 = skew_into_positive_orthant(D, nparam, MaxRays);
     for (Polyhedron *P = D2; P; P = P->next) {
 	assert(P->Dimension == D2->Dimension);
-	POL_ENSURE_VERTICES(P);
-	/* it doesn't matter which reducer we use, since we don't actually
-	 * reduce anything here
-	 */
-	partial_reducer red(Polyhedron_Project(P, P->Dimension), P->Dimension, 
-			    P->Dimension);
-	red.start(P, MaxRays);
+	gen_fun *P_gf;
+
+	P_gf = series(Polyhedron_Copy(P), nparam, MaxRays);
 	if (!gf)
-	    gf = red.gf;
+	    gf = P_gf;
 	else {
-	    gf->add_union(red.gf, MaxRays);
-	    delete red.gf;
+	    gf->add_union(P_gf, MaxRays);
+	    delete P_gf;
 	}
     }
     /* we actually only need the convex union of the parameter space

commit 3b0eaac0403c303c7a655e39f86b75c7971ea689

barvinok_enumerate_union_series_with_options: fix typo
This typo was introduced way back in 323ee0f57c8583146b0ecd737217671b98b4896d
and results in completely meaningless barvinok_union output
(reported by Harald Devos).

Also add a test case for (non-parametric) barvinok_union.

barvinok.cc
@@ -3441,7 +3441,7 @@ gen_fun* barvinok_enumerate_union_series_with_options(Polyhedron *D, Polyhedron*
 	assert(P->Dimension == D2->Dimension);
 	gen_fun *P_gf;
 
-	P_gf = series(Polyhedron_Copy(P), nparam, options);
+	P_gf = series(Polyhedron_Copy(P), P->Dimension, options);
 	if (!gf)
 	    gf = P_gf;
 	else {

test.c
@@ -34,7 +34,7 @@ static void time_diff(struct tms *before, struct tms *after)
 
 int main(int argc, char **argv)
 {
-    int i, nbPol, nbVec, nbMat, func, j;
+    int i, nbPol, nbVec, nbMat, func, j, n;
     Polyhedron *A, *B, *C, *D, *E, *F, *G;
     char s[128];
     struct barvinok_options *options = barvinok_options_new_with_defaults();
@@ -173,8 +173,42 @@ int main(int argc, char **argv)
 	    Domain_Free(B);
 	    Domain_Free(C);
 	    break;
+	case 10: {
+	    evalue *EP;
+	    Value cb, ck;
+
+	    value_init(cb);
+	    value_init(ck);
+	    fgets(s, 128, stdin);
+	    sscanf(s, "%d", &n);
+	    for (j = 0; j < n; ++j) {
+		Polyhedron *P;
+		M = Matrix_Read();
+		P = Constraints2Polyhedron(M, options->MaxRays);
+		Matrix_Free(M);
+		A = DomainConcat(P, A);
+	    }
+	    fgets(s, 128, stdin);
+	    /* workaround for apparent bug in older gmps */
+	    *strchr(s, '\n') = '\0';
+	    while ((*s=='#') || (value_read(ck, s) != 0)) {
+		fgets(s, 128, stdin);
+		/* workaround for apparent bug in older gmps */
+		*strchr(s, '\n') = '\0';
+	    }
+	    C = Universe_Polyhedron(0);
+	    EP = barvinok_enumerate_union(A, C, options->MaxRays);
+	    value_set_double(cb, compute_evalue(EP, &ck)+.25);
+	    if (value_ne(cb, ck))
+		return -1;
+	    Domain_Free(C);
+	    value_clear(cb);
+	    value_clear(ck);
+	    free_evalue_refs(EP);
+	    free(EP);
 	}
-	Polyhedron_Free(A);
+	}
+	Domain_Free(A);
     }
     for (i = 0; i < nbVec; ++i) {
 	int ok;

tests/union
@@ -1,0 +1,16 @@
+D 1
+4 4
+#  i  j  cst 
+1  1  0   -1  # i >= 1
+1  0  1   -1  # j >= 1
+1 -1  0    8  # i <= 8
+1  0 -1   12  # j <= 12
+F 10
+1
+4 4
+#  i  j  cst 
+1  1  0  -1  # i >= 1
+1  0  1  -1  # j >= 1
+1 -1  0  10  # i <= 10
+1  0 -1  10  # j <= 10
+116