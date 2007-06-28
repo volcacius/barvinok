cb987bd5a5079690c491aa927a96e35a4567f529
Sven Verdoolaege <skimo@kotnet.org>
2007-06-28 14:09:14 +0200

    polysign.h: use correct name of PL_polyhedron_affine_sign in prototype

--- polysign.h
+++ polysign.h
@@ -12,7 +12,7 @@ enum order_sign { order_lt, order_le, order_eq, order_ge, order_gt, order_unknow
 /* Returns the sign of the affine function specified by T on the polyhedron D */
 enum order_sign polyhedron_affine_sign(Polyhedron *D, Matrix *T,
 					    struct lexmin_options *options);
-enum order_sign polylib_polyhedron_affine_sign(Polyhedron *D, Matrix *T,
+enum order_sign PL_polyhedron_affine_sign(Polyhedron *D, Matrix *T,
 					    struct barvinok_options *options);
 enum order_sign cdd_polyhedron_affine_sign(Polyhedron *D, Matrix *T,
 					    struct barvinok_options *options);
