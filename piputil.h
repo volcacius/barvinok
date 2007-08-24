eb263c06f49b18a58067c187a833a0789ca324d4
Sven Verdoolaege <skimo@kotnet.org>
2007-08-24 18:21:36 +0200

    barvinok.cc: split off part dealing with existential variables
    The code is mostly left as is, even though it is in serious need
    of clean-up.

--- piputil.h
+++ piputil.h
@@ -1,3 +1,11 @@
 #include <barvinok/polylib.h>
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 Polyhedron *pip_projectout(Polyhedron *P, int pos, int n, int nparam);
+
+#if defined(__cplusplus)
+}
+#endif
