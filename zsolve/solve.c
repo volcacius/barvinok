6b47f420d676b50a3f85da77d6e6b5bcb484e9d8
Sven Verdoolaege <skimo@kotnet.org>
2007-09-26 21:42:46 +0200

    add 4ti2-1.3.1+autoconf-0.1 banner

--- zsolve/solve.c
+++ zsolve/solve.c
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #include <ctype.h>
 #include <assert.h>
 
-#include "../banner.h"
+#include "banner.h"
 #include "opts.h"
 #include "libzsolve.h"
 
