df41ee1fafaf4abe08c640f66658d8cd4972a5b0
Sven Verdoolaege <skimo@kotnet.org>
2007-09-30 12:32:55 +0200

    barvinok/polylib.h: remove compatibility with older versions of PolyLib

--- barvinok/polylib.h
+++ barvinok/polylib.h
@@ -9,23 +9,8 @@ extern "C" {
 
 #include <polylib/polylibgmp.h>
 
-#ifndef value_subtract
-#define value_subtract	value_substract
-#endif
-
 #define	value_gcd(ref,val1,val2)	mpz_gcd(ref,val1,val2)
 #define	value_lcm(ref,val1,val2)	mpz_lcm(ref,val1,val2)
-
-#ifndef value_addmul
-#define value_addmul(ref,val1,val2)						\
-	    do {								\
-		Value _tmp;							\
-		value_init(_tmp);						\
-		value_multiply(_tmp,val1,val2);				    	\
-		value_addto(ref,ref,_tmp);					\
-		value_clear(_tmp);						\
-	    } while(0)
-#endif
 
 #undef divide
 #undef value_compare
