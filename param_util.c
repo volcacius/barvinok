volcacius/barvinok

commit 6db34c2f2553bec44993b002e63d9bc6844bccda

Use gmp's gcd rather than PolyLib's
The gmp version appears to be an order of magnitude faster.
Also replace value_lcm by gmp's version.

param_util.c
@@ -19,7 +19,7 @@ void Param_Vertex_Common_Denominator(Param_Vertices *V)
 
     value_assign(lcm, V->Vertex->p[0][dim+1]);
     for (i = 1; i < V->Vertex->NbRows; ++i)
-	value_lcm(V->Vertex->p[i][dim+1], lcm, &lcm);
+	value_lcm(lcm, V->Vertex->p[i][dim+1], lcm);
 
     for (i = 0; i < V->Vertex->NbRows; ++i) {
 	if (value_eq(V->Vertex->p[i][dim+1], lcm))

barvinok.cc
@@ -276,9 +276,9 @@ static void mask_table(Matrix *f, evalue *factor)
 	    continue;
 	for (int j = 0; j < np; ++j)
 	    if (value_notzero_p(f->p[n][j])) {
-		Gcd(f->p[n][j], f->p[n][nc-1], &tmp);
+		value_gcd(tmp, f->p[n][j], f->p[n][nc-1]);
 		value_division(tmp, f->p[n][nc-1], tmp);
-		value_lcm(tmp, lcm->p[j], &lcm->p[j]);
+		value_lcm(lcm->p[j], tmp, lcm->p[j]);
 	    }
     }
     evalue EP;

barvinok/polylib.h
@@ -13,6 +13,9 @@ extern "C" {
 #define value_subtract	value_substract
 #endif
 
+#define	value_gcd(ref,val1,val2)	mpz_gcd(ref,val1,val2)
+#define	value_lcm(ref,val1,val2)	mpz_lcm(ref,val1,val2)
+
 #ifndef value_addmul
 #define value_addmul(ref,val1,val2)						\
 	    do {								\

barvinok/util.h
@@ -42,7 +42,6 @@ extern "C" {
 
 struct barvinok_options;
 
-void value_lcm(const Value i, const Value j, Value* lcm);
 int random_int(int max);
 Polyhedron *Polyhedron_Read(unsigned MaxRays);
 Polyhedron* Polyhedron_Polar(Polyhedron *P, unsigned NbMaxRays);

barvinok_e.cc
@@ -471,7 +471,7 @@ static evalue* enumerate_sure(Polyhedron *P,
 		continue;
 	    if (value_one_p(S->Constraint[j][1+nvar+i]))
 		continue;
-	    value_lcm(lcm, S->Constraint[j][1+nvar+i], &lcm);
+	    value_lcm(lcm, lcm, S->Constraint[j][1+nvar+i]);
 	}
 
 	for (int j = 0; j < S->NbConstraints; ++j) {

bernoulli.c
@@ -64,15 +64,15 @@ struct bernoulli_coef *bernoulli_coef_compute(int n)
 	    value_addto(bernoulli_coef.num->p[i], bernoulli_coef.num->p[i], tmp);
 	}
 	mpz_mul_ui(bernoulli_coef.den->p[i], bernoulli_coef.lcm->p[i-1], i+1);
-	Gcd(bernoulli_coef.num->p[i], bernoulli_coef.den->p[i], &tmp);
+	value_gcd(tmp, bernoulli_coef.num->p[i], bernoulli_coef.den->p[i]);
 	if (value_notone_p(tmp)) {
 	    value_division(bernoulli_coef.num->p[i],
 			    bernoulli_coef.num->p[i], tmp);
 	    value_division(bernoulli_coef.den->p[i],
 			    bernoulli_coef.den->p[i], tmp);
 	}
-	value_lcm(bernoulli_coef.lcm->p[i-1], bernoulli_coef.den->p[i],
-		  &bernoulli_coef.lcm->p[i]);
+	value_lcm(bernoulli_coef.lcm->p[i],
+		  bernoulli_coef.lcm->p[i-1], bernoulli_coef.den->p[i]);
     }
     bernoulli_coef.n = n+1;
     value_clear(factor);

bernstein.cc
@@ -176,7 +176,7 @@ static bool evalue_is_periodic(const evalue *e, Vector *periods)
 	Value size;
 	value_init(size);
 	value_set_si(size, e->x.p->size);
-	value_lcm(periods->p[e->x.p->pos-1], size, &periods->p[e->x.p->pos-1]);
+	value_lcm(periods->p[e->x.p->pos-1], periods->p[e->x.p->pos-1], size);
 	value_clear(size);
 	is_periodic = true;
     }
@@ -565,8 +565,8 @@ static piecewise_lst *bernstein_coefficients_periodic(piecewise_lst *pl_all,
     value_set_si(T2->p[D->Dimension][D->Dimension], 1);
     for (int i = 0; i < D->Dimension; ++i) {
 	value_assign(T->p[i][i], periods->p[i]);
-	value_lcm(T2->p[D->Dimension][D->Dimension], periods->p[i],
-		  &T2->p[D->Dimension][D->Dimension]);
+	value_lcm(T2->p[D->Dimension][D->Dimension],
+		  T2->p[D->Dimension][D->Dimension], periods->p[i]);
     }
     value_set_si(T->p[D->Dimension][D->Dimension], 1);
     for (int i = 0; i < D->Dimension; ++i)

edomain.cc
@@ -188,7 +188,7 @@ static void add_coeff(Value *cons, int len, evalue *coeff, int pos)
 
     value_init(tmp);
 
-    value_lcm(cons[0], coeff->d, &tmp);
+    value_lcm(tmp, cons[0], coeff->d);
     value_division(tmp, tmp, cons[0]);
     Vector_Scale(cons, cons, tmp, len);
     value_division(tmp, cons[0], coeff->d);

commit 9c2b0cff677c3fd8c9c35c46286aff12913e525b

Param_Inner_Product: fix use of value_lcm
It was broken in 6db34c2f2553bec44993b002e63d9bc6844bccda.

param_util.c
@@ -56,7 +56,7 @@ void Param_Inner_Product(Value *constraint, Matrix *Vertex, Value *row)
 	value_assign(tmp2,  constraint[1+j]);
 	if (value_ne(row[0], Vertex->p[j][nparam+1])) {
 	    value_assign(tmp, row[0]);
-	    value_lcm(row[0], Vertex->p[j][nparam+1], &row[0]);
+	    value_lcm(row[0], row[0], Vertex->p[j][nparam+1]);
 	    value_division(tmp, row[0], tmp);
 	    value_multiply(tmp2, tmp2, row[0]);
 	    value_division(tmp2, tmp2, Vertex->p[j][nparam+1]);