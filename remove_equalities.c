eeb8f9292639c61774838b279fa58b560a5288ea
Sven Verdoolaege <skimo@kotnet.org>
2007-10-20 15:48:08 +0200

    remove_all_equalities: minor code clean-up

--- remove_equalities.c
+++ remove_equalities.c
@@ -21,6 +21,20 @@ static void transform(Polyhedron **P, Polyhedron **C, Matrix *CP, int free,
 	Polyhedron_Free(D);
 }
 
+static Matrix *compose_transformations(Matrix *first, Matrix *second)
+{
+    Matrix *combined;
+
+    if (!first)
+	return second;
+
+    combined = Matrix_Alloc(first->NbRows, second->NbColumns);
+    Matrix_Product(first, second, combined);
+    Matrix_Free(first);
+    Matrix_Free(second);
+    return combined;
+}
+
 /* Remove all equalities in P and the context C (if not NULL).
  * Does not destroy P (or C).
  * Returns transformation on the parameters in the Matrix pointed to by CPP
@@ -50,9 +64,9 @@ int remove_all_equalities(Polyhedron **P, Polyhedron **C, Matrix **CPP, Matrix *
 
     if (D && D->NbEq) {
 	Polyhedron_Matrix_View(D, &M, D->NbEq);
-	CV = compress_variables(&M, 0);
-	transform(&Q, &D, CV, Q != *P, MaxRays);
-	nparam = CV->NbColumns-1;
+	CP = compress_variables(&M, 0);
+	transform(&Q, &D, CP, Q != *P, MaxRays);
+	nparam = CP->NbColumns-1;
     }
 
     /* compress_parms doesn't like equalities that only involve parameters */
@@ -68,49 +82,41 @@ int remove_all_equalities(Polyhedron **P, Polyhedron **C, Matrix **CPP, Matrix *
 
     if (i < Q->NbEq) {
 	Matrix *M = Matrix_Alloc(Q->NbEq, 1+nparam+1);
+	Matrix *T;
 	int n = 0;
+
 	for (; i < Q->NbEq; ++i) {
 	    if (First_Non_Zero(Q->Constraint[i]+1, Q->Dimension-nparam) == -1)
 		Vector_Copy(Q->Constraint[i]+1+Q->Dimension-nparam,
 			    M->p[n++]+1, nparam+1);
 	}
 	M->NbRows = n;
-	CV = compress_variables(M, 0);
+	T = compress_variables(M, 0);
 	Matrix_Free(M);
-	transform(&Q, &D, CV, Q != *P, MaxRays);
-	nparam = CV->NbColumns-1;
+	transform(&Q, &D, T, Q != *P, MaxRays);
+	nparam = T->NbColumns-1;
+	CP = compose_transformations(CP, T);
     }
 
     if (emptyQ2(Q)) {
-	if (CV)
-	    Matrix_Free(CV);
+	if (CP)
+	    Matrix_Free(CP);
 	*P = Q;
 	if (C)
 	    *C = D;
 	return 0;
     }
 
-    if (Q->NbEq == 0) {
-	CP = CV;
-	CV = NULL;
-    } else {
+    if (Q->NbEq) {
+	Matrix *T;
 	Polyhedron_Matrix_View(Q, &M, Q->NbEq);
-	CP = compress_parms(&M, nparam);
+	T = compress_parms(&M, nparam);
 
-	if (isIdentity(CP)) {
-	    Matrix_Free(CP);
-	    CP = CV;
-	    CV = NULL;
+	if (isIdentity(T)) {
+	    Matrix_Free(T);
 	} else {
-	    transform(&Q, &D, CP, Q != *P, MaxRays);
-	    if (CV) {
-		Matrix *T = CP;
-		CP = Matrix_Alloc(CV->NbRows, T->NbColumns);
-		Matrix_Product(CV, T, CP);
-		Matrix_Free(T);
-		Matrix_Free(CV);
-		CV = NULL;
-	    }
+	    transform(&Q, &D, T, Q != *P, MaxRays);
+	    CP = compose_transformations(CP, T);
 	    nparam = CP->NbColumns-1;
 	}
 
