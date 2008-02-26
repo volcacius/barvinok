785340e4bcd421c3af9f03fee7c8ea4c2d0165fa
Sven Verdoolaege <skimo@kotnet.org>
2008-02-26 18:26:45 +0100

    lattice_point.h: export fractional_part with C linkage

--- lattice_point.h
+++ lattice_point.h
@@ -2,9 +2,22 @@
 #define LATTICE_POINT_H
 
 #include <gmp.h>
+#include <barvinok/polylib.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+evalue *fractional_part(Value *coef, Value denom, int nvar, Polyhedron *PD);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#if defined(__cplusplus)
+
 #include <NTL/vec_ZZ.h>
 #include <NTL/mat_ZZ.h>
-#include <barvinok/polylib.h>
 
 #ifdef NTL_STD_CXX
 using namespace NTL;
@@ -14,7 +27,6 @@ struct barvinok_options;
 
 evalue *multi_monom(vec_ZZ& p);
 int normal_mod(Value *coef, int len, Value *m);
-evalue *fractional_part(Value *coef, Value denom, int nvar, Polyhedron *PD);
 void lattice_point_fixed(Value *vertex, Value *vertex_res,
 			 Matrix *Rays, Matrix *Rays_res,
 			 Value *point);
@@ -38,5 +50,7 @@ struct term_info {
 void lattice_point(Param_Vertices* V, const mat_ZZ& rays, vec_ZZ& lambda,
     term_info* term, unsigned long det,
     barvinok_options *options);
+
+#endif
 
 #endif
