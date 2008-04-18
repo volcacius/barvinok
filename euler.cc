308d52dd6b8b2d2b6ab1ac1ff127694c69f3f8de
Sven Verdoolaege <skimo@kotnet.org>
2008-04-18 23:42:52 +0200

    power.h: extract from euler.cc

--- euler.cc
+++ euler.cc
@@ -12,6 +12,7 @@
 #include "euler.h"
 #include "lattice_point.h"
 #include "param_util.h"
+#include "power.h"
 #include "reduce_domain.h"
 
 /* Compute total degree in first nvar variables */
@@ -97,30 +98,6 @@ Value *binomial(unsigned n, unsigned k)
     binom.n = n+1;
     return &binom.binom[n]->p[k];
 }
-
-struct power {
-    int	     n;
-    Vector  *powers;
-
-    power(Value v, int max) {
-	powers = Vector_Alloc(max+1);
-	assert(powers);
-	value_set_si(powers->p[0], 1);
-	if (max >= 1)
-	    value_assign(powers->p[1], v);
-	n = 2;
-    }
-    ~power() {
-	Vector_Free(powers);
-    }
-    Value *operator[](int exp) {
-	assert(exp >= 0);
-	assert(exp < powers->Size);
-	for (; n <= exp; ++n)
-	    value_multiply(powers->p[n], powers->p[n-1], powers->p[1]);
-	return &powers->p[exp];
-    }
-};
 
 /*
  * Computes the coefficients of
