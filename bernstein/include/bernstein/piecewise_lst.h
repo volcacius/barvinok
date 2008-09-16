a071a325dcd587fc9ad4b3da0962013349bc6810
Sven Verdoolaege <skimo@kotnet.org>
2008-09-16 15:46:43 +0200

    piecewise_lst.h: avoid duplicate parameter name

--- bernstein/include/bernstein/piecewise_lst.h
+++ bernstein/include/bernstein/piecewise_lst.h
@@ -30,7 +30,7 @@ struct piecewise_lst {
     void minimize();
     void simplify_domains(Polyhedron *ctx, unsigned MaxRays);
     GiNaC::numeric evaluate(const GiNaC::exvector& values);
-    void evaluate(int n, Value *v, Value *n, Value *d);
+    void evaluate(int n, Value *v, Value *num, Value *d);
     GiNaC::numeric evaluate(const GiNaC::exvector& values, int n, Value *v);
     void add(const GiNaC::ex& poly);
     int is_equal(const piecewise_lst& other) const;
