bcce040cca82f089e853ac892173925b52fe0d30
Sven Verdoolaege <skimo@kotnet.org>
2011-01-15 15:37:31 +0100

    iscc: support application of piecewise quasipolynomial (fold) on set
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- iscc.c
+++ iscc.c
@@ -1173,15 +1173,24 @@ static struct isl_obj apply_fun(struct isl_stream *s,
 	struct isl_obj arg;
 
 	arg = read_expr(s, table);
-	isl_assert(s->ctx, is_subtype(arg, isl_obj_union_map), goto error);
+	if (!is_subtype(arg, isl_obj_union_map) &&
+	    !is_subtype(arg, isl_obj_union_set))
+		isl_die(s->ctx, isl_error_invalid,
+			"expecting set of map argument", goto error);
 
 	if (arg.type == isl_obj_list) {
 		struct isl_list *list = arg.v;
 		if (list->n == 2 && list->obj[1].type == isl_obj_bool)
 			arg = obj_at(arg, 0);
 	}
-	if (arg.type == isl_obj_map)
+	if (arg.type == isl_obj_set)
+		arg = convert(s->ctx, arg, isl_obj_union_set);
+	else if (arg.type == isl_obj_map)
 		arg = convert(s->ctx, arg, isl_obj_union_map);
+	if (arg.type == isl_obj_union_set) {
+		arg.v = isl_union_map_from_range(arg.v);
+		arg.type = isl_obj_union_map;
+	}
 	if (obj.type == isl_obj_union_pw_qpolynomial) {
 		obj.v = isl_union_map_apply_union_pw_qpolynomial(arg.v, obj.v);
 	} else {
