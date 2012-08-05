2c83456eb780097f77e9b188edb005f938b76f09
Sven Verdoolaege <skimo@kotnet.org>
2012-08-05 13:02:48 +0200

    export isl_*_pw_qpolynomial_sum
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- barvinok/isl.h
+++ barvinok/isl.h
@@ -22,8 +22,10 @@ __isl_give isl_union_pw_qpolynomial *isl_union_set_card(
 __isl_export
 __isl_give isl_union_pw_qpolynomial *isl_union_map_card(
 	__isl_take isl_union_map *umap);
+__isl_export
 __isl_give isl_pw_qpolynomial *isl_pw_qpolynomial_sum(
 	__isl_take isl_pw_qpolynomial *pwqp);
+__isl_export
 __isl_give isl_union_pw_qpolynomial *isl_union_pw_qpolynomial_sum(
 	__isl_take isl_union_pw_qpolynomial *upwqp);
 __isl_give isl_pw_qpolynomial *isl_set_apply_pw_qpolynomial(
